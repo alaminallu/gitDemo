0c5c871d8eef97e75e14b92365865a98fd37c49f
Muhammad Al amin <alamin33667788@gmail.com>
2025-08-16 23:29:25 +0600

    new update in all file

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alert("Hello World!");
+let loginBox= document.getElementById("login-form");
+let regBox= document.getElementById("register-form");
+
+let toggleBtn= document.getElementById("toggle-button");
+let toggleText= document.getElementById("toggle-text");
+
+let isloginpage = true;
+
+toggleBtn.addEventListener("click",() =>{
+
+
+    if(isloginpage){
+
+        loginBox.style.display="block";
+        toggleText.innerHTML="Don,t have an account?"
+        toggleBtn.innerHTML="Register";
+        regBox.style.display="none";
+        isloginpage= false;
+
+    }else{
+        regBox.style.display="block";
+        toggleText.innerHTML="Allrady have an account?"
+        toggleBtn.innerHTML="Login";
+        loginBox.style.display="none";
+        isloginpage= true;
+    }
+
+});
